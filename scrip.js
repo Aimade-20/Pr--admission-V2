139f5ae4a328dcee647e098b7ac3d5d4b3916c0e
Aimade safsafi <imadesafsafi8@gmail.com>
2026-01-28 10:41:35 +0100

    pre-admission v2

---
+++ scrip.js
@@ -0,0 +1,121 @@
+let array = [];
+let nomeEl = document.getElementById("nom");
+let prenomEl = document.getElementById("prenom");
+let telephonEl = document.getElementById("telephon");
+let EmailEl = document.getElementById("email");
+let MotifEl = document.getElementById("Motif");
+let dateEl = document.getElementById("date");
+let tbody = document.getElementById("table");
+
+
+function ajouter() {
+  let ajout = {
+    nome: nomeEl.value,
+    prenom: prenomEl.value,
+    telephon: telephonEl.value,
+    Email: EmailEl.value,
+    Motif: MotifEl.value,
+    date: dateEl.value,
+  };
+  let para = document.getElementById("messag");
+  if (
+    nomeEl.value == "" ||
+    prenomEl.value == "" ||
+    telephonEl.value == "" ||
+    EmailEl.value == "" ||
+    MotifEl.value == "" ||
+    dateEl.value == ""
+  ) {
+    para.textContent = "Veuillez compléter les champs obligatoires.";
+    para.className = "error";
+    para.classList.add("show");
+    hide();
+  } else {
+    array.push(ajout);
+    
+    para.textContent = "Demande ajoutée avec succès";
+    para.className = "success";
+    para.classList.add("show");
+    hide();
+    afichTable();
+  }
+  console.log(array);
+}
+afichTable();
+const ajouterbtn = document.querySelector("#ajouter");
+ajouterbtn.addEventListener("click", () => ajouter());
+
+function afichTable(data = array) {
+  tbody.innerHTML = "";
+
+  if (!data.length) {
+    tbody.innerHTML = `
+      <tr>
+        <td colspan="7" style="text-align:center; padding:30px;">
+          Aucun résultat
+        </td>
+      </tr>
+    `;
+    return;
+  }
+
+  data.forEach((item, index) => {
+    let tr = document.createElement("tr");
+    tr.innerHTML = `
+      <td>${item.nome}</td>
+      <td>${item.prenom}</td>
+      <td>${item.telephon}</td>
+      <td>${item.Email}</td>
+      <td>${item.Motif}</td>
+      <td>${item.date}</td>
+      <td>
+        <button class="supri" onclick="suprimerBut(${index})">
+          supprimer
+        </button>
+      </td>
+    `;
+    tbody.appendChild(tr);
+  });
+  contentDeme ()
+}
+function suprimerBut(index) {
+  array.splice(index, 1);
+  afichTable();
+}
+
+function hide() {
+  setTimeout(() => {
+    document.getElementById("messag").classList.remove("show");
+  }, 3000);
+}
+
+
+function searchFilter() {
+  const vall = document.getElementById("search").value.toLowerCase();
+  if (!vall) {
+  afichTable(array);
+  return;
+}
+  const fil = array.filter(
+    (item) =>
+      item.nome.toLowerCase().includes(vall) ||
+      item.prenom.toLowerCase().includes(vall) ||
+      item.telephon.includes(vall) ||
+      item.Email.toLowerCase().includes(vall),
+  );
+
+  
+  afichTable(fil);
+}
+document
+  .getElementById("search")
+  .addEventListener("input", searchFilter);
+ 
+  function contentDeme (){
+      document.getElementById("nomber");
+      array.length +"demend au total"
+
+  }
+
+  
+
